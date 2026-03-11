e111e3764719b5a79d5e3edf0f4841b7ed49c5e4
jydx <jeremydenaux@gmail.com>
2026-03-11 20:00:11 +0000

    Hey yeah first commit

---
+++ ft_strlen.c
@@ -0,0 +1,21 @@
+/*Header*/
+
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return (i);
+}
+
+int main(void)
+{
+    char test[25] = "Coucou les genoux";
+
+    printf("Longueur super array : %d\n", ft_strlen(test));
+    return (0);
+}
