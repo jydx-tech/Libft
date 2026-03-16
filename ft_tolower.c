635de9316c069982007bc06cab69619d84e62dc4
jydx <jeremydenaux@gmail.com>
2026-03-16 16:45:53 +0000

    save

---
+++ ft_tolower.c
@@ -0,0 +1,23 @@
+/*header*/
+
+#include "libft.h"
+
+int ft_tolower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return (c + 32);
+    return (c);
+}
+
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int res;
+    if (argc == 2)
+    {
+        res = ft_tolower((argv[1][0]));
+        printf("lower = %c \n", res);
+    }
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -13,6 +13,8 @@ int ft_isdigit(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
+int ft_toupper(int c);
+int ft_tolower(int c);
 
 /*strings*/
 size_t  ft_strlen(const char *str);
