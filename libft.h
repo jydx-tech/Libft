105efbd45112b7997e2326c9655d691297bf528d
jydx <jeremydenaux@gmail.com>
2026-03-17 21:04:35 +0000

    save

--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char *ft_strchr(const char *s, int c);
 int ft_strncmp( const char * s1, const char * s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 
 /* memory */
 void	ft_bzero(void *s, size_t n);
@@ -31,4 +32,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
+/*convertion*/
+int	ft_atoi(const char *str);
 #endif
