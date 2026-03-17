cb195d221e1bca37eda87887f3fe4e53f4736061
jydx <jeremydenaux@gmail.com>
2026-03-17 08:22:58 +0000

    save

--- ft_strchr.c
+++ ft_strchr.c
@@ -2,7 +2,33 @@
 
 #include "libft.h"
 
-char *strchr(const char *s, int c)
+char *ft_strchr(const char *s, int c)
 {
-    
+    int i;
+
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == (char)c)
+            return ((char *)&s[i]);
+        i++;
+    }
+    if ( (char)c== '\0')
+        return ((char *)&s[i]);
+    return (NULL);
 }
+/*
+int main(int argc, char **argv)
+{
+    char *res;
+
+    if (argc == 3)
+    {
+        res = ft_strchr(argv[1], argv[2][0]);
+        printf("string: %s\n", argv[1]);
+        printf("to search: %c\n", argv[2][0]);
+        res = ft_strchr(argv[1], argv[2][0]);
+        printf("res: %s\n", res);
+    }
+    return (0);
+}*/
--- libft.h
+++ libft.h
@@ -20,6 +20,7 @@ int ft_tolower(int c);
 size_t  ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char *ft_strchr(const char *s, int c);
 
 /* memory */
 void	ft_bzero(void *s, size_t n);
