27330e6d6629f8d79b9019775bb97d396009b574
jydx <jeremydenaux@gmail.com>
2026-03-17 16:13:38 +0000

    save

---
+++ ft_memchr.c
@@ -0,0 +1,38 @@
+/*header*/
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+    size_t  i;
+    unsigned char *str;
+
+    i = 0;
+    str = (unsigned char *)s;
+    while (i < n)
+    {
+        if (str[i] == (unsigned char)c)
+            return ((void *)&str[i]);
+        i++;
+    }
+    return (NULL);
+}
+/*
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    void *res;
+
+    if (argc == 4)
+    {
+        res = ft_memchr(argv[1], argv[2][0], atoi(argv[3]));
+        printf("str : %s\n", argv[1]);
+        printf("to search : %c\n", argv[2][0]);
+        if(res)
+            printf("res : %s\n", (char *)res);
+        else
+            printf("not here!!!\n");
+    }
+    return (0);
+}*/
