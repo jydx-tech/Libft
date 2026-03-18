99d4cda7903e5b9376eaee9b16b41c8cd3c602af
jydx <jeremydenaux@gmail.com>
2026-03-18 18:18:19 +0000

    save

--- libft.h
+++ libft.h
@@ -27,6 +27,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
 
 /* memory */
 void	ft_bzero(void *s, size_t n);
