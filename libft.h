8d3997c8e70d56b2c39e1b5c972f46eb15092ccd
jydx <jeremydenaux@gmail.com>
2026-03-19 20:30:39 +0000

    save

--- libft.h
+++ libft.h
@@ -45,4 +45,10 @@ void *ft_calloc( size_t elementcount, size_t elementsize);
 int	ft_atoi(const char *str);
 char *ft_itoa(int n);
 
+/*display*/
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
+void ft_putendl_fd(char *s, int fd);
+
 #endif
