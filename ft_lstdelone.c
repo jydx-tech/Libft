1c8d0903b2f409431d3097cacf8f6c17663ec71e
jydx <jeremydenaux@gmail.com>
2026-04-25 14:58:21 +0000

    save

---
+++ ft_lstdelone.c
@@ -0,0 +1,13 @@
+/*header*/
+
+#include "libft.h"
+
+void ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    if (!lst || !del)
+        return;
+    del(lst -> content);
+    free (lst);
+}
+/*On utilise la fonction 'del' pour libérer le contenu du nœud
+on free le noeud*/
